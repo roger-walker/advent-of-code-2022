2f4a5b915a7915237dc896de51e6a7a7f203a973
Roger Walker <roger.walker@hyland.com>
2022-12-01 17:16:34 -0500

    day 1 solutions for elves carrying food

---
+++ AdventOfCode2022/Day1.cs
@@ -0,0 +1,71 @@
+﻿using System.IO;
+
+namespace AdventOfCode2022
+{
+    internal class Day1
+    {
+        List<Elf> elves = new List<Elf>();
+
+        public string FileName { get; set; }
+
+        public Day1(string fileName)
+        {
+            FileName = fileName;
+            Setup();
+        }
+
+        public void Setup()
+        {
+            using (StreamReader sr = File.OpenText(FileName))
+            {
+                string? line;
+
+                Elf curr = new Elf();
+                while ((line = sr.ReadLine()) != null)
+                {
+                    if (!string.IsNullOrEmpty(line))
+                    {
+                        curr.CalorieCount += Convert.ToInt32(line);
+                    }
+                    else
+                    {
+                        elves.Add(curr);
+                        curr = new Elf();
+                    }
+                }
+                elves.Add(curr);
+            }
+        }
+
+        public int Max()
+        {
+            return elves.Max(e => e.CalorieCount);
+        }
+
+        public int TopThree()
+        {
+            elves.Sort();
+            elves.Reverse();
+            return elves.Take(3).Aggregate(0, (acc, e) => acc + e.CalorieCount);
+        }
+
+        internal class Elf : IComparable<Elf>
+        {
+            public int CalorieCount { get; set; }
+
+            public int CompareTo(Elf? other)
+            {
+                if (other == null)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return this.CalorieCount.CompareTo(other.CalorieCount);
+                }
+            }
+        }
+
+    }
+
+}
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using AdventOfCode2022;
+
+
+string input = "../../../Inputs/day1-1.txt";
+
+Day1 day1 = new Day1(input);
+
+int maxElf = day1.Max();
+Console.WriteLine($"Max Cal: {maxElf}");
+
+int top3 = day1.TopThree();
+Console.WriteLine($"Top Three: {top3}");
