b046b1f6e4ce863a022cc58ce2c4c82d2d381fa9
Roger Walker <roger.walker@hyland.com>
2022-12-02 17:10:37 -0500

    Set up for tomorrows stuff...

---
+++ AdventTests/Day3Tests.cs
@@ -0,0 +1,19 @@
+﻿using AdventOfCode2022;
+
+namespace AdventTests
+{
+    internal class Day3Tests
+    {
+        [Setup]
+        public void Setup { }
+
+
+        [Test]
+        public void DegenerateCase_DoesNothing()
+        {
+            Day3 day3 = new Day3();
+
+            Assert.That(true);
+        }
+    }
+}
