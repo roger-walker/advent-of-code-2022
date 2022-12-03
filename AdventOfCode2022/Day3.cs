e924998ef59c71ed4c829fe7a86b9a962383972b
Roger Walker <roger.john.walker@gmail.com>
2022-12-03 15:37:57 -0500

    Solved Part 1 of day 3

--- AdventOfCode2022/Day3.cs
+++ AdventOfCode2022/Day3.cs
@@ -8,5 +8,64 @@ namespace AdventOfCode2022
 {
     public class Day3
     {
+        public List<string> Rucksacks { get; set; }
+
+        public Day3()
+        {
+            Rucksacks = new List<string>();
+        }
+
+        public Day3(List<string> rucks)
+        {
+            Rucksacks = rucks;
+        }
+
+        public List<string> SplitIntoCompartments(string rucksack)
+        {
+            int half = rucksack.Length / 2;
+            string comp1  = rucksack.Substring(0, half);
+            string comp2  = rucksack.Substring(half);
+
+            return new List<string>() { comp1, comp2 };
+        }
+
+        public string FindMatchingItem(string comp1, string comp2)
+        {
+            string match = "";
+
+            foreach(var item in comp1)
+            {
+                if (comp2.Any(x => x == item))
+                {
+                    match = "" +  item;
+                }
+            }
+
+            return match;
+        }
+
+        public int FindItemPriority(string item)
+        {
+            char ch = item[0];
+
+            return Char.IsLower(ch) ? ch - 'a' + 1 : ch - 'A' + 27;
+        }
+
+        public int FindRucksackPriority(string rucksack)
+        {
+            var comps = SplitIntoCompartments(rucksack);
+            var item = FindMatchingItem(comps);
+            return FindItemPriority(item);
+        }
+
+        private string FindMatchingItem(List<string> comps)
+        {
+            return FindMatchingItem(comps[0], comps[1]);
+        }
+
+        public int FindTotalPriority()
+        {
+            return Rucksacks.Aggregate(0, (acc, c) => acc + FindRucksackPriority(c));
+        }
     }
 }
