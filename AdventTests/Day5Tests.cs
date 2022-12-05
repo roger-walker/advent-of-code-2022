513d1940176fd28db50b93e72a1b40e2ed314dcb
Roger Walker <roger.john.walker@gmail.com>
2022-12-04 20:31:41 -0500

    setting up for tomorrow

---
+++ AdventTests/Day5Tests.cs
@@ -0,0 +1,21 @@
+﻿using AdventOfCode2022;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventTests
+{
+    internal class Day5Tests
+    {
+        [Test]
+        public void Total_Degenerate()
+        {
+            Day5 day = new Day5();
+
+            Assert.That(day, Is.Not.Null);
+        }
+
+    }
+}
