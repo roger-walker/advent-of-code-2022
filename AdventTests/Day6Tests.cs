a2b4f435cb25fb66359c5bc5e6035695ce69d03c
Roger Walker <roger.john.walker@gmail.com>
2022-12-05 20:36:43 -0500

    ready to go

---
+++ AdventTests/Day6Tests.cs
@@ -0,0 +1,16 @@
+﻿using AdventOfCode2022;
+
+namespace AdventTests
+{
+    internal class Day6Tests
+    {
+        [Test]
+        public void blah()
+        {
+            Day6 day = new Day6();
+
+            Assert.That(day, Is.Not.Null);
+        }
+
+    }
+}
