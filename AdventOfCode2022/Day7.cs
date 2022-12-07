14068bfbc99b2b7697624b26f99e625c0259ac7d
Roger Walker <roger.walker@hyland.com>
2022-12-07 14:02:11 -0500

    parse filesize

---
+++ AdventOfCode2022/Day7.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    public class Day7
+    {
+        public int FindFileSize(string file)
+        {
+            int fileSize = 0;
+            var fileInfo = file.Split(' ');
+            fileSize = Convert.ToInt32(fileInfo[0]);
+
+            return fileSize;
+        }
+    }
+}
---
+++ AdventTests/Day7Tests.cs
@@ -0,0 +1,57 @@
+﻿using AdventOfCode2022;
+
+
+
+
+namespace AdventTests
+{
+
+    /*
+    $ cd /
+    $ ls
+    dir a
+    14848514 b.txt
+    8504156 c.dat
+    dir d
+    $ cd a
+    $ ls
+    dir e
+    29116 f
+    2557 g
+    62596 h.lst
+    $ cd e
+    $ ls
+    584 i
+    $ cd ..
+    $ cd ..
+    $ cd d
+    $ ls
+    4060174 j
+    8033020 d.log
+    5626152 d.ext
+    7214296 k
+     */
+
+
+
+
+    internal class Day7Tests
+    {
+        [Test]
+        [TestCase("0 a", 0)]
+        [TestCase("1 a", 1)]
+        [TestCase("14848514 b.txt", 14848514)]
+        [TestCase("8504156 c.dat", 8504156)]
+        [TestCase("29116 f", 29116)]
+        [TestCase("2557 g", 2557)]
+        public void GivenFileInput_DetermineFileSize(string file, int expected)
+        {
+            Day7 day = new Day7();
+
+            var actual = day.FindFileSize(file);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+    }
+}
