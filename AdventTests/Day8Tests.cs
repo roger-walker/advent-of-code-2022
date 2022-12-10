f47faf1bec0afa427a94efb1c5e30354c64b81b4
Roger Walker <roger.john.walker@gmail.com>
2022-12-10 10:14:44 -0500

    setup

---
+++ AdventTests/Day8Tests.cs
@@ -0,0 +1,22 @@
+﻿using AdventOfCode2022;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventTests
+{
+    internal class Day8Tests
+    {
+
+        [Test]
+        public void blah()
+        {
+            Day8 day = new Day8();
+
+            Assert.That(day, Is.Not.Null);
+        }
+
+    }
+}
